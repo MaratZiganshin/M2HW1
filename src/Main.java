805f2a5db8e9d81eae872e806a945fb148fafac8
Marat Ziganshin <zig-marat@mail.ru>
2017-11-16 14:24:13 +0300

    generate numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
+    public static ArrayList<Long> generateNumbers(int N){
+        Random rnd = new Random();
+        ArrayList<Long> result = new ArrayList<>();
+        for (int i = 0; i < N; i++){
+            long ins = 0;
+            do {
+                ins = rnd.nextLong();
+            } while (result.contains(ins));
+            result.add(ins);
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<Long> test = generateNumbers(50);
+        for (long a : test)
+            System.out.println(a);
     }
 }
